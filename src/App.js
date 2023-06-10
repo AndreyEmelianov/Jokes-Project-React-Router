b59010d52a4791b4dfbc6ca6c00b4c8e5ef03636
Andrey Emelianov <621077@mail.ru>
2023-06-10 16:01:09 +0300

    добавил лаяут

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
 
+import Layout from './components/layout/Layout';
 import Jokes from './pages/Jokes';
 import JokeDetails from './pages/JokeDetails';
 import AddJoke from './pages/AddJoke';
 
 function App() {
 	return (
-		<Switch>
-			<Route path="/" exact>
-				<Redirect to="/jokes" />
-			</Route>
-			<Route path="/jokes" exact>
-				<Jokes />
-			</Route>
-			<Route path="/jokes/:jokeId">
-				<JokeDetails />
-			</Route>
-			<Route path="/add-joke">
-				<AddJoke />
-			</Route>
-		</Switch>
+		<Layout>
+			<Switch>
+				<Route path="/" exact>
+					<Redirect to="/jokes" />
+				</Route>
+				<Route path="/jokes" exact>
+					<Jokes />
+				</Route>
+				<Route path="/jokes/:jokeId">
+					<JokeDetails />
+				</Route>
+				<Route path="/add-joke">
+					<AddJoke />
+				</Route>
+			</Switch>
+		</Layout>
 	);
 }
 
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,20 +1,25 @@
+import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
 import styles from './Header.module.css';
 
 const Header = () => {
-  return (
-    <header className={styles.header}>
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <a href='/'>Jokes</a>
-          </li>
-          <li>
-            <a href='/'>Add a Joke</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+	return (
+		<header className={styles.header}>
+			<nav className={styles.nav}>
+				<ul>
+					<li>
+						<NavLink to="/jokes" activeClassName={styles.active}>
+							Jokes
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/add-joke" activeClassName={styles.active}>
+							Add a Joke
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 };
 
 export default Header;
